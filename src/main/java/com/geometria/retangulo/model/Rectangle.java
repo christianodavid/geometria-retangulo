3c0443eed91dfe5af4ef737b19bec35535108f5f
Christiano <christianodvids@gmail.com>
2025-03-15 16:40:26 -0300

    feat: implement Rectangle model with area and perimeter calculations

---
+++ src/main/java/com/geometria/retangulo/model/Rectangle.java
@@ -0,0 +1,35 @@
+package com.geometria.retangulo.model;
+
+public class Rectangle {
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public double calculateArea() {
+        return sideA * sideB;
+    }
+
+    public double calculatePerimeter() {
+        return (2 * sideA) + (2 * sideB);
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+}
